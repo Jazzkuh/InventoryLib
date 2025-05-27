4dd1365eb15ae8f903d278268fa768dd30f05b02
Daan <35238334+TheBathDuck@users.noreply.github.com>
2025-05-27 23:28:20 +0200

    update minestom

--- minestom/src/main/java/com/jazzkuh/inventorylib/listeners/ButtonInventoryClickListener.java
+++ minestom/src/main/java/com/jazzkuh/inventorylib/listeners/ButtonInventoryClickListener.java
@@ -8,7 +8,6 @@ import net.minestom.server.entity.Player;
 import net.minestom.server.event.EventListener;
 import net.minestom.server.event.inventory.InventoryPreClickEvent;
 import net.minestom.server.inventory.click.Click;
-import net.minestom.server.inventory.click.ClickType;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.sound.SoundEvent;
 import net.minestom.server.tag.Tag;
